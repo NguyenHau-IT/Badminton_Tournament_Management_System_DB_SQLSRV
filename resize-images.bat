a61d4131c886a02863a0180920884e4c3a602fb5
NGUYỄN VIỆT HẬU <nguyenviethau.it.2004@gmail.com>
2025-09-30 09:27:11 +0700

    Update resize-images.bat

--- resize-images.bat
+++ resize-images.bat
@@ -8,7 +8,7 @@ echo Đang compile và chạy công cụ resize ảnh...
 echo.
 
 REM Compile Java file
-javac -cp "lib/*" src/main/java/com/example/badmintoneventtechnology/util/image/ImageResizer.java
+javac -cp "lib/*" src/main/java/com/example/btms/util/image/ImageResizer.java
 
 if %ERRORLEVEL% NEQ 0 (
     echo Lỗi khi compile! Vui lòng kiểm tra Java và dependencies.
@@ -20,7 +20,7 @@ echo Compile thành công!
 echo.
 
 REM Chạy công cụ resize
-java -cp "src/main/java;lib/*" com.example.badmintoneventtechnology.util.image.ImageResizer
+java -cp "src/main/java;lib/*" com.example.btms.util.image.ImageResizer
 
 echo.
 echo ========================================
